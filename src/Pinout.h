28a1a33cc026640944a3106974fc587edc02412b
OscarGLorenz <oscar.glorenz@alumnos.upm.es>
2017-02-11 17:32:57 +0100

    Cambios estructura directorios para Atom-PlatformIO
    	nuevo archivo: src/GyroCTRL.h
    	nuevo archivo: src/Pinout.h
    	nuevo archivo: src/cybertech17-exg.ino

---
+++ src/Pinout.h
@@ -0,0 +1,38 @@
+/*
+ * Pinout.h
+ *
+ *  Created on: 4 feb. 2017
+ *      Author: oscar
+ */
+
+#ifndef PINOUT_H_
+#define PINOUT_H_
+
+#define BLUE_LED	1
+#define BUTTON	0
+
+#define PIN1_QTR	3
+#define PIN2_QTR	4
+#define PIN3_QTR	5
+#define PIN4_QTR	7
+#define PIN5_QTR	8
+#define PIN6_QTR	12
+#define PIN7_QTR	13
+#define PIN8_QTR	A0
+
+#define FRONT_SHARP	A6
+#define BACK_SHARP	A7
+#define LEFT_SHARP	A2
+#define RIGHT_SHARP	A3
+
+#define INTERRUPT_GYRO	2
+
+#define PWM1A	9
+#define PWM1B	6
+#define PWM2A	10
+#define PWM2B	11
+
+#define BAT_VOLTAGE A1
+
+
+#endif /* PINOUT_H_ */
